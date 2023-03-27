876b623dcc1ef7310714a8e50ec2d8e51aa1e469
Hileene <linebeh@gmail.com>
2023-03-27 22:26:45 +0200

    Mise en place du routage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Lodging from "./pages/Lodging/Lodging"
+import About from "./pages/About/About"
+import Error from "./pages/Error/Error"
+
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lodging/:id" element={<Lodging/>}/>
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<Error/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
