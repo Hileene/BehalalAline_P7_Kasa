beea064907184797b5c8f66ecfdc5ca6cf9fdb1f
Hileene <linebeh@gmail.com>
2023-03-29 21:43:44 +0200

    Mise en forme du header

---
+++ src/Layouts/Header/Header.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom"
+import Logo from '../../assets/logo.png'
+import "./header.css"
+
+function Header() {
+    return (
+        <header>
+            <img src={Logo} alt="Logo Kasa" className="logo-header" />
+            <nav>
+                <NavLink to="/home" className="nav-links">Accueil</NavLink>
+                <NavLink to="/about" className="nav-links">A Propos</NavLink>
+            </nav>
+
+        </header>
+    )
+}
+
+export default Header
