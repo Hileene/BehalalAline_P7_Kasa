04ed6e938ad0d7ba28ed0b91cbd994880ade0894
Hileene <linebeh@gmail.com>
2023-04-04 14:09:24 +0200

    Nouvelle configuration de la structure du router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Lodging from './pages/Lodging/Lodging'
-import About from './pages/About/About'
-import Error from './pages/Error/Error'
-import Layout from "./layout/Layout"
-
+import RouterPath from './router/RouterPath'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/lodging/:id" element={<Lodging />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/*" element={<Error />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
+      <RouterPath/>
     </div>
   )
 }
