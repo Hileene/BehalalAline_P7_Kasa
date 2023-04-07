a1960353b268bf45da1594fc4eee4b2919930a41
Hileene <linebeh@gmail.com>
2023-04-07 11:57:49 +0200

    Mise en forme du footer

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -5,9 +5,9 @@ import LogoFooter from "../../assets/logo-footer.png";
 function Footer() {
     return(
         <footer>
-            <div>
-                <img src={LogoFooter} alt="Logo Kasa" />
-                <p>&copy; 2022 Kasa. Tous droits réservés</p>
+            <div className="kasa-footer">
+                <img src={LogoFooter} alt="Logo Kasa" className="logo-footer" />
+                <p className="copyrights-text">&copy; 2022 Kasa. Tous droits réservés</p>
             </div>
         </footer>
     );
