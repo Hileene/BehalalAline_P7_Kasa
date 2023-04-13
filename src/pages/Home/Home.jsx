e50eadcfbe64d41557279c3884444a1238f48c65
Hileene <linebeh@gmail.com>
2023-04-13 22:06:08 +0200

    Ajout de Gallery

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from "../../components/Banner/Banner"
 import HomeImg from "../../assets/home-img.png"
+import Gallery from "../../components/Gallery/Gallery"
 
 function Home() {
     return(
         <div>
-            <Banner image={HomeImg} text= "Chez vous, partout et ailleurs" />   
+            <Banner image={HomeImg} text= "Chez vous, partout et ailleurs" /> 
+            <Gallery />  
         </div>
+       
+
     )
 }
 
