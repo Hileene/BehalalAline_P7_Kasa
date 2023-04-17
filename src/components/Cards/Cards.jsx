4ecf4d471ba5cb092bcba0907716e29432bf21a1
Hileene <linebeh@gmail.com>
2023-04-17 17:27:34 +0200

    Ajout du composant router Link

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,13 +1,16 @@
 import './cards.css'
+import {Link} from "react-router-dom"
 
 
 function Cards({ id, cover, title }) {
   return (
+    <Link to={`/lodgings/${id}`}>
     <div className="cards-items" id={id}>
             <img className="cards-cover" src={cover} alt="{title}"/>
             <div className="cards-darker"></div>
             <span className="cards-title">{title}</span>
         </div>
+    </Link>
   )
 }
 
