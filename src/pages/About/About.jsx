ba954d7b082081bef1c59b61de027e1b4f7793e6
Hileene <linebeh@gmail.com>
2023-04-19 17:37:08 +0200

    Mise en place de la structure

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,25 @@
-function About() {
-    return(
-        <div></div>
-    )
+import Banner from "../../components/Banner/Banner"
+import AboutImg from "../../assets/about-img.png"
+import {ValuesDatas} from "../../datas/ValuesDatas.jsx"
+import Collapse from "../../components/Collapse/Collapse"
+import "./about.css"
+
+ function About() {
+  
+  return (
+    <div>
+      <Banner image={AboutImg} title="" />
+      <main className="about">
+        {ValuesDatas.map((data) => {
+          return (
+            <div key={data.id} className="about_main_collapse">
+              <Collapse title={data.title} text={data.text} />
+            </div>
+          )
+        })}
+      </main>
+    </div>
+  )
 }
 
 export default About
