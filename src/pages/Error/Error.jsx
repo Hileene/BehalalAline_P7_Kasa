e51c5811b68f267aa1c8bbb7ab0f65b5f7908b55
Hileene <linebeh@gmail.com>
2023-04-23 20:45:30 +0200

    Création de la structure

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,23 @@
+import ErrorImg from '../../assets/404.png'
+import { NavLink } from 'react-router-dom'
+import './error.css'
+
 function Error() {
-    return(
-        <div>404</div>
-    )
+  return (
+    <div className="error">
+      <img
+        src={ErrorImg}
+        alt="Cette page est introuvable"
+        className="error-pic"
+      />
+      <span className="error-text">
+        Oups! La page que vous demandez n'existe pas
+      </span>
+      <NavLink className="error-redirect" to="/">
+       Retourner sur la page d'accueil
+      </NavLink>
+    </div>
+  )
 }
 
 export default Error
