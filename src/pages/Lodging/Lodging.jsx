71bb71e707b77cfa9fe960aa8a5c75e4273be889
Hileene <linebeh@gmail.com>
2023-05-02 10:55:55 +0200

    Importation du caroussel

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -1,6 +1,20 @@
-export default function Lodging() {
-    return(
-        <div>Logements</div>
-    )
-}
+import Slideshow from '../../components/Slideshow/Slideshow'
+import Lodgings from '../../datas/lodgings.json'
+import { useParams } from 'react-router-dom'
 
+
+export default function Lodging() {
+  const { id } = useParams()
+  const foundLodgings = Lodgings.find((object) => object.id === id)
+  const pictures = foundLodgings.pictures
+  
+
+  return (
+    <>
+      <Slideshow slides={pictures} />
+      <main className='lodgings'>
+        
+      </main>
+    </>
+  )
+}
