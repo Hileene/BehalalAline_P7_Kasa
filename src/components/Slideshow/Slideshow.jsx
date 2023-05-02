5d137df7da2233be5e4432eeb69e0b1761c6507d
Hileene <linebeh@gmail.com>
2023-05-02 10:55:20 +0200

    Finalisation du caroussel

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -4,10 +4,7 @@ import ArrowBack from '../../assets/arrow-back.png'
 import './slideshow.css'
 
 export default function Slideshow({ slides }) {
-  
-
   const [current, setCurrent] = useState(0)
-  
 
   const nextSlide = () => {
     setCurrent(current === slides.length - 1 ? 0 : current + 1)
@@ -17,31 +14,36 @@ export default function Slideshow({ slides }) {
     setCurrent(current === 0 ? slides.length - 1 : current - 1)
   }
 
-  if (!Array.isArray(slides) || slides.length <= 0) {
-    return null
+  if (slides.length > 1) {
+    return (
+      <section className="slider">
+        <img className="slider-img" src={slides[current]} alt={slides.description} />
+        <img
+          className="arrow-back"
+          src={ArrowBack}
+          alt="Slide précédente"
+          onClick={prevSlide}
+        />
+        <img
+          className="arrow-forward"
+          src={ArrowForward}
+          alt="Slide suivante"
+          onClick={nextSlide}
+        />
+        <span className="slide-number">
+          {current + 1} / {slides.length}
+        </span>
+      </section>
+    )
+  } else {
+    return(
+
+    <section className="slider">
+    <img src={slides[current]} alt={slides.description} />
+      <span className="slide-number">
+        {current + 1} / {slides.length}
+      </span>
+    </section>
+    )
   }
-
-  return (
-    <section className='slider'>
-            {slides.length > 1 && 
-                <>
-                    <img 
-                        className='arrow-back' 
-                        src={ArrowBack} 
-                        alt="Slide précédente" 
-                        onClick={prevSlide}
-                    />
-                    <img 
-                        className='arrow-forward' 
-                        src={ArrowForward} 
-                        alt="Slide suivante" 
-                        onClick={nextSlide}
-                    />
-                    <span className='slide-number'>{current + 1} / {slides.length}</span>
-                </>
-            } 
-        </section>
-  )
 }
-
-
