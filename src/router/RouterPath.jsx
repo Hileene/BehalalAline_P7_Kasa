6bfdc2a936b7e9e6723e48186eb3de6f73e10f15
Hileene <92981899+Hileene@users.noreply.github.com>
2024-01-05 14:20:15 +0100

    Mise à jour de "BrowseRouter"  avec "basename"

--- src/router/RouterPath.jsx
+++ src/router/RouterPath.jsx
@@ -1,26 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from "../pages/Home/Home"
+import Home from '../pages/Home/Home'
 import Lodging from '../pages/Lodging/Lodging'
 import About from '../pages/About/About'
 import Error from '../pages/Error/Error'
-import Layout from "../layout/Layout"
+import Layout from '../layout/Layout'
 
-
-//Ce composant configure les itinéraires (routes) de navigation à l'aide 
+//Ce composant configure les itinéraires (routes) de navigation à l'aide
 //du système de routage de React Router.
 function RouterPath() {
-    return (
-        <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/lodging/:id" element={<Lodging />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/*" element={<Error />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    )
+  return (
+    <BrowserRouter basename="/kasa">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lodging/:id" element={<Lodging />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
 }
 
 export default RouterPath
