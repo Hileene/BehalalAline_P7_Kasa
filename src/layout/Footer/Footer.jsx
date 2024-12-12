0630248acb9bdc717cf370523a01d08a09ed4b88
Hileene <92981899+Hileene@users.noreply.github.com>
2024-12-12 15:08:39 +0100

    Ajout d'un lien vers github répertoire

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-import "./footer.css";
-import LogoFooter from "../../assets/logo-footer.png";
+import React from 'react'
+import './footer.css'
+import LogoFooter from '../../assets/logo-footer.png'
 
 function Footer() {
-    return(
-        <footer>
-            <div className="kasa-footer">
-                <img src={LogoFooter} alt="Logo Kasa" className="logo-footer" />
-                <p className="copyrights-text">&copy; 2022 Kasa. Tous droits réservés</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="kasa-footer">
+        <img src={LogoFooter} alt="Logo Kasa" className="logo-footer" />
+        <p className="copyrights-text">
+          &copy; 2023{' '}
+          <a
+            href="https://github.com/Hileene/BehalalAline_P7_Kasa/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-link"
+          >
+            Aline Behalal
+          </a>{' '}
+          - Tous droits reservés.
+        </p>
+      </div>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
